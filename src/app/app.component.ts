6bda41cb8e5791121f1963777cbb93d0c188ac33
Misha3739 <mihail.udot@yandex.ru>
2017-08-13 17:33:57 +0300

    Task completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -23,10 +24,37 @@ export class AppComponent implements OnInit{
   {
     this.projectForm = new FormGroup(
       {
-        'projectname': new FormControl(null, Validators.required ),
-        'email': new FormControl(null, [ Validators.email, Validators.required ]),
+        'projectname': new FormControl(null, [Validators.required, this.forbiddenProjectNames.bind(this)] ),
+        'email': new FormControl(null, [  Validators.required, Validators.email ], this.forbiddenEmails),
         'status': new FormControl(null)
       }
     );
+  }
+
+  forbiddenProjectNames(control: FormControl): {[s: string]: boolean}
+  {
+    if(control.value === 'Test')
+    {
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any>
+  {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value.startsWith('Test') || control.value.startsWith('test'))
+        {
+          resolve({'emailIsForbidden' : true});
+        }
+        else
+        {
+          resolve(null);
+        }
+
+      }, 1500);
+    });
+    return promise;
   }
 }
